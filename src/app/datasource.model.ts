5fd2699b5274235317df8670a8da14996f34cc3a
ishikis <ishik.is@hotmail.com>
2019-09-02 16:45:25 +0300

    ..

--- src/app/datasource.model.ts
+++ src/app/datasource.model.ts
@@ -6,11 +6,12 @@ export class SimpleDataSource {
     constructor(
     ) {
         this.product = new Array<Product>(
-            new Product(1, "Samsung s5", "iyi telefon", "1.jpeg", 1000),
-            new Product(2, "Samsung s6", "iyi telefon", "1.jpeg", 2000),
-            new Product(3, "Samsung s7", "iyi telefon", "1.jpeg", 3000),
-            new Product(4, "Samsung s8", "iyi telefon", "1.jpeg", 4000),
-            new Product(5, "Samsung s9", "iyi telefon", "1.jpeg", 5000),
+            new Product(1, "Samsung s4", "iyi telefon", "s4.jpg", 1000),
+            new Product(2, "Samsung s5", "iyi telefon", "1.jpeg", 1000),
+            new Product(3, "Samsung s6", "iyi telefon", "1.jpeg", 2000),
+            new Product(4, "Samsung s7", "iyi telefon", "1.jpeg", 3000),
+            new Product(5, "Samsung s8", "iyi telefon", "1.jpeg", 4000),
+            new Product(6, "Samsung s9", "iyi telefon", "1.jpeg", 5000),
         );
     }
     getProducts(): Product[] {
--- src/app/product.model.ts
+++ src/app/product.model.ts
@@ -1,9 +1,9 @@
 export class Product {
-    constructor (
-    public id? :number,
-    public name?:string,
-    public description?:string,
-    public imageUrl? : string,
-    public price? : number,
-        ){}
+    constructor(
+        public id?: number,
+        public name?: string,
+        public description?: string,
+        public imageUrl?: string,
+        public price?: number,
+    ) { }
 }
